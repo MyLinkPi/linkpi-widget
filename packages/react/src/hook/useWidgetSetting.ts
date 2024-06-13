de6d0405a7e1956d57c55b5d752ba03dc44cae5a
1-week <bayeszou@gmail.com>
2024-06-13 17:16:31 +0800

    fix(react): use singleton context

--- packages/react/src/hook/useWidgetSetting.ts
+++ packages/react/src/hook/useWidgetSetting.ts
@@ -1,13 +1,30 @@
 import { UnknownObject } from "@mylinkpi/widget-core";
 import { createContext, useContext } from "react";
 
-export const WidgetSettingContext = createContext({
-  value: {},
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  setValue: (_newValue: UnknownObject) => {},
-});
+const hackKey = "__mylinkpi__fixme__widget_setting_context__";
+
+/**
+ * 返回单例的 context
+ */
+export const getWidgetSettingContext = <
+  T extends UnknownObject = UnknownObject,
+>() => {
+  if (!window[hackKey as any]) {
+    // @ts-ignore
+    window[hackKey as any] = createContext({
+      value: {},
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      setValue: (_newValue: T) => {},
+    });
+  }
+  return window[hackKey as any] as unknown as React.Context<{
+    value: T;
+    setValue: (_newValue: T) => void;
+  }>;
+};
 
 export const useWidgetSetting = <T extends UnknownObject = UnknownObject>() => {
+  const WidgetSettingContext = getWidgetSettingContext();
   const { value, setValue } = useContext(WidgetSettingContext);
 
   return [value as T, setValue as (arg: T) => void] as const;
