ed84a11ab8a9b350873160ea6e389e316f31e29e
Bo <bo1806@163.com>
2024-11-05 17:54:19 +0800

    fix: import path error

--- packages/cli/src/commands/info.ts
+++ packages/cli/src/commands/info.ts
@@ -1,10 +1,9 @@
 import { Command, Flags } from "@oclif/core";
 
-import { loginLinkPi } from "@/utils/login.js";
-
 import { getWidget } from "../services/index.js";
 import { getWidget as getDevWidget } from "../services/index-dev.js";
 import { getWidgetConfig } from "../utils/index.js";
+import { loginLinkPi } from "../utils/login.js";
 
 export default class Info extends Command {
   static override description = "print the widget info";
